357de79f3933eb471a1c0bd094c332225880c823
sinezplered <sinezplered@gmail.com>
2025-07-15 04:30:36 +0700

    Add files via upload

---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Bokep Artis Indo';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
+export const url = 'https://bokepartisindo.pages.dev';
+export const terbit = '2025-01-28T00:01:03Z';
+
+export const google = 'URxJLGYKFTF1JHqPnZZpjeCVERLvWQeIppwnuddpxtk';
+export const yandex = '232fa79f1e944f53'
+
+export const index = '15c7102a-2882-4e69-8a41-b30a0ed8981a';
