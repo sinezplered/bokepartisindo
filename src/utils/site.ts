e40801629a95cc8ddd22053e2e89738cbb1b3392
sinezplered <sinezplered@gmail.com>
2025-07-15 04:29:27 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Artis Indo';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
-export const url = 'https://bokepartisindo.pages.dev';
-export const terbit = '2025-01-28T00:01:03Z';
